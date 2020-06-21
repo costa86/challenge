39918d59688a3c506b248205a3cafaee90312ac4
costa86 <costa86@zoho.com>
2020-06-21 16:49:00 +0100

    Updates

---
+++ precache-manifest.6b0617c2fe63601ba14ea30b9cbf5d03.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "054a35cdbed2c070378bd2404d9d36b1",
+    "url": "/challenge/index.html"
+  },
+  {
+    "revision": "7438303cae61f18ef4be",
+    "url": "/challenge/static/css/main.acd6e2a3.chunk.css"
+  },
+  {
+    "revision": "942ecb4b12b49d936402",
+    "url": "/challenge/static/js/2.6960e76f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/challenge/static/js/2.6960e76f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7438303cae61f18ef4be",
+    "url": "/challenge/static/js/main.c836b212.chunk.js"
+  },
+  {
+    "revision": "dec300b6f2c8caf38593",
+    "url": "/challenge/static/js/runtime-main.1587758d.js"
+  },
+  {
+    "revision": "81943c62f6d1f2bdad38ac520d2c9501",
+    "url": "/challenge/static/media/logo.81943c62.svg"
+  }
+]);
