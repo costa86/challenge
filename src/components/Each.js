513ce9c1823003b1bee710a268c9f0d9fa36412e
costa86 <costa86@zoho.com>
2020-06-21 15:15:59 +0100

    set missing img

--- src/components/Each.js
+++ src/components/Each.js
@@ -4,14 +4,30 @@ export function Each(props) {
 
     const myRound = num => (+num).toFixed(2).toString();
 
-    let image = `./../assets/${props.item.pair.slice(0,3)}.png`;
+    let currencyConverted = myRound(props.amount / props.item.bid);
+
+    let currencyName = props.item.pair.slice(0, 3);
+
+    let currencyImage = `./../assets/${currencyName}.png`;
+
+    let noCurrencyImage = `./../assets/XPT.png`;
+
+    function setImg(source, defaultImg){
+        let url = source;
+        let img = new Image();
+        img.src = url;
+        if (img.width === 0){
+            url = defaultImg; 
+        }
+        return url;
+    }
 
     return (
         <>
             <div className="card">
-                <h3>{myRound(props.amount / props.item.bid)}</h3>
-                <h3>{props.item.pair.slice(0, 3)}</h3>
-                <img alt="dad" src={image}></img>
+                <h3>{currencyConverted}</h3>
+                <h3>{currencyName}</h3>
+                <img alt={currencyName} src={setImg(currencyImage,noCurrencyImage)}></img>
             </div>
 
         </>
